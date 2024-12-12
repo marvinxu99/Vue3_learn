830ff68ec7d11f5a42cc71ca2030383a45fa9a5d
Marvin Xu <marvinxu99@hotmail.com>
2024-12-11 19:30:36 -0800

    ..

--- my-vue3-app/src/views/AboutView.vue
+++ my-vue3-app/src/views/AboutView.vue
@@ -1,13 +1,21 @@
 <script setup lang="ts">
 import TheExample from '../components/ExampleComponent.vue'
+import HelloWorld from '../components/HelloWorld.vue'
+import CounterComponent from '../components/CounterComponent.vue'
 </script>
 
 <template>
   <main>
+    <h2>TheExample Component</h2>
     <TheExample />
+    <br />
+    <h2>CounterComponent</h2>
+    <CounterComponent />
+    <br />
     <div class="about">
       <h1>This is an about page</h1>
     </div>
+    <HelloWorld msg="Test Msg" />
   </main>
 </template>
 
