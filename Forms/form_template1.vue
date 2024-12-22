235479c0671142a7c052651adb6bcb64bd597424
Marvin Xu <marvinxu99@hotmail.com>
2024-12-22 12:04:06 -0800

    ...

---
+++ Forms/form_template1.vue
@@ -0,0 +1,12 @@
+<form action="/dataCollectionLocation" method="post" autocomplete="on">
+    <div v-for="item in formItems" :key="item.id" class="form-item">
+      <label :for="item.id">{{ item.label }}: </label>
+      <input
+        :type="item.type"
+        :id="item.id"
+        :name="item.id"
+        v-model="item.value"
+      />
+    </div>
+    <button type="submit">Submit</button>
+  </form>
